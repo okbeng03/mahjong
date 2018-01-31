b3f906a3211266c6fc353477c1485298ad56ee58
changbin.wangcb <changbin.wangcb@alibaba-inc.com>
2018-01-31 20:25:08 +0800

    计分

--- src/rules/bonus.ts
+++ src/rules/bonus.ts
@@ -1,10 +1,36 @@
 import * as _ from 'lodash';
-import Bonus from '../bonus';
+// import Bonus from '../bonus';
 import Round from '../round';
 import Player from '../playerDetail';
 import { MeldDetail } from '../meld';
 import { ClaimType, sortTiles, Card } from '../tile';
 import { groupByType, groupSize } from './basic';
+
+// 胡牌类型
+export enum WinType {
+  CommonHand = 1, // 平胡
+  Uniform,        // 清一色
+  Pong,           // 碰碰胡
+  Pair,           // 七小对
+  LuxuryPair,     // 豪华七小对
+  Uniq            // 十三幺
+};
+
+// 奖励类型
+export enum BonusType {
+  Win,            // 胡
+  SelfDraw,       // 自摸
+  Kong,           // 杠上开花
+  Sky,            // 天胡
+  Land,           // 地胡
+  FlowerSeason,   // 花胡
+  FlowerBotany,   // 花胡
+  Expose,         // 明杠
+  ConcealedKong,  // 暗杠
+  BaoPai,         // 包牌
+  Cannon,         // 放炮
+  FirstFollow     // 首张被跟
+};
 
 const thirteenOrphans = [
   Card.CharacterOne,
@@ -22,33 +48,104 @@ const thirteenOrphans = [
   Card.White
 ];
 
-export function calculate(round: Round): Bonus[] {
+export function calculate(round: Round): void {
   const players = round.players;
-  const winner = round.winner;
-  const bonus: Bonus[] = _.fill(Array(players.length), {
-    score: 0,
-    selfDraw: 0,
-    win: 0,
-    expose: 0,
-    concealedKong: 0,
-    cannon: 0
+  const winner = players[round.winner];
+  const banker = round.game.banker;
+  const bankerCount = round.game.bankerCount + 1;
+  const cannons: number[] = _.fill(Array(players.length), 0);
+
+  players.forEach(function(player) {
+    checkKong(player, players, cannons);
+    checkFlower(player);
   });
+  
+  const winScore = checkWin(winner, players, cannons);
+  let isBaoPai = winner.fourMeld;
 
-  players.forEach(function(player, i) {
-    checkKong(player, bonus);
+  if (winner.threeMeld && winner.winFrom === winner.threeMeld) {
+    isBaoPai = winner.threeMeld;
+  }
+  
+  // 包牌
+  if (isBaoPai > -1) {
+    compute(players, banker, bankerCount);
+    players[isBaoPai].bonus.push(BonusType.BaoPai);
+    let base = 1;
+    let score = 0;
 
-    if (winner === i) {
-      checkWin(player, bonus);
+    if (winner.pick === banker) {
+      base = bankerCount;
     }
-    
-    checkFlower(player, bonus);
-  });
 
-  return bonus;
+    players.forEach(function(player, i) {
+      if (i === winner.pick) {
+        return;
+      }
+
+      if (i === banker) {
+        base = bankerCount;
+      }
+
+      score += winScore * bankerCount;
+    });
+
+    winner.score += score;
+    players[isBaoPai].score -= score;
+  } else {
+    winner.score += winScore;
+    compute(players, banker, bankerCount);
+  }
+
+  subtractCannon(players, cannons);
 }
 
-function checkWin(player: Player, bonus: Bonus[]): void {
-  const { win, winFrom, chowTiles, handTiles, pick } = player;
+// 计算分数
+function compute(players: Player[], banker: number, bankerCount: number) {
+  const len = players.length;
+
+  for (let i = 0; i < len; i++) {
+    const player = players[i];
+
+    if (player.score <= 0) {
+      continue;
+    }
+
+    let base = 1;
+    
+    if (i === banker) {
+      base = bankerCount;
+    }
+
+    for (let j = 0; j < len; j++) {
+      if (j === i) {
+        continue;
+      }
+
+      if (j === banker) {
+        base = bankerCount;
+      }
+
+      let s = player.score * base;
+      player.score += s;
+      players[j].score -= s;
+    }
+  }
+}
+
+// 减去放炮
+function subtractCannon(players: Player[], cannons: number[]) {
+  cannons.forEach(function(cannon, i) {
+    players[i].score -= cannon;
+  });
+}
+
+// 计算胡的牌型和分数
+function checkWin(player: Player, players: Player[], cannons: number[]): number {
+  const { bonus, winFrom, chowTiles, handTiles } = player;
+  const winType = bonus.slice().sort(function(a: number, b: number): number {
+    return a - b;
+  })[0];
   let base = 1;
 
   // 检查牌型
@@ -56,10 +153,13 @@ function checkWin(player: Player, bonus: Bonus[]): void {
   chowTiles.forEach(function(meld: MeldDetail) {
     tiles = tiles.concat(meld.tiles);
   });
+
+  player.winType = WinType.CommonHand;
   
   // 清一色
   if (checkOneType(tiles)) {
     base += 1;
+    player.winType = WinType.Uniform;
   }
 
   const size = groupSize(handTiles);
@@ -67,37 +167,49 @@ function checkWin(player: Player, bonus: Bonus[]): void {
   // 碰碰胡
   if (checkAllPong(chowTiles, size)) {
     base += 1;
+    player.winType = WinType.Pong;
   }
 
   if (handTiles.length === 14) {
     if (checkPair(size)) {
       // 七小对
       base += 1;
+      player.winType = WinType.Pair;
     } else if (checkLuxuryPair(size)) {
       // 豪华七小对
       base += 2;
+      player.winType = WinType.LuxuryPair;
     } else if (checkUniq(handTiles)) {
       // 十三幺
       base += 12;
+      player.winType = WinType.Uniq;
     }
   }
 
   if (winFrom > -1) {
-    bonus[winFrom].score -= 1;
-    bonus[winFrom].cannon = 1;
+    players[winFrom].bonus.push(BonusType.Cannon);
+    cannons[winFrom] -= 1;
   }
 
-  switch(win) {
-    case ClaimType.Win:
-      bonus[pick].score *= 1;
+  switch(winType) {
+    case BonusType.Win:
+      base *= 1;
       break;
-    case ClaimType.SelfDraw:
-      bonus[pick].score *= 2;
+    case BonusType.SelfDraw:
+      base *= 2;
       break;
-    case ClaimType.Kong: 
-      bonus[pick].score *= 3;
+    case BonusType.Kong: 
+      base *= 3;
+      break;
+    case BonusType.Sky:
+      base *= 3;
+      break;
+    case BonusType.Land:
+      base *= 3;
       break;
   }
+
+  return base;
 }
 
 // 清一色
@@ -138,28 +250,31 @@ function checkUniq(tiles: number[]): boolean {
 }
 
 // 花胡
-function checkFlower(player: Player, bonus: Bonus[]): void {
-  if (player.flowerWin) {
-    bonus[player.pick].win = 1;
-    bonus[player.pick].score += player.flowerWin;
+function checkFlower(player: Player): void {
+  if (_.indexOf(player.bonus, BonusType.FlowerSeason) > -1) {
+    player.score += 1;
+  }
+
+  if (_.indexOf(player.bonus, BonusType.FlowerBotany) > -1) {
+    player.score += 1;
   }
 }
 
 // 杠
-function checkKong(player: Player, bonus: Bonus[]): void {
+function checkKong(player: Player, players: Player[], cannons: number[]): void {
   player.chowTiles.forEach(function(meld: MeldDetail) {
     const pick = player.pick;
 
     if (meld.type === ClaimType.Expose) {
-      bonus[pick].expose++;
-      bonus[pick].score += 1;
-      bonus[meld.from].score -= 1;
-      bonus[meld.from].cannon = 1;
+      player.score += 1;
+      player.bonus.push(BonusType.Expose);
+      players[meld.from].bonus.push(BonusType.Cannon);
+      cannons[meld.from] -= 1;
     }
 
     if (meld.type === ClaimType.ConcealedKong) {
-      bonus[pick].concealedKong++;
-      bonus[pick].score += 2;
+      player.score += 2;
+      player.bonus.push(BonusType.ConcealedKong);
     }
   });
 }
