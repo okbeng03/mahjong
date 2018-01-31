7444304445e436cf6aa031cdf94631c734f1fba7
changbin.wangcb <changbin.wangcb@alibaba-inc.com>
2018-01-31 20:17:28 +0800

    change bonus type

--- src/player.ts
+++ src/player.ts
@@ -1,22 +1,11 @@
-import Bonus from './bonus';
-
 export default class Player {
   readonly id: number;
   readonly name: string;
   readonly pick: number;
-  bonus: Bonus;
 
   constructor(id: number, name: string, pick: number) {
     this.id = id;
     this.name = name;
     this.pick = pick;
-    this.bonus = {
-      score: 0,
-      selfDraw: 0,
-      win: 0,
-      expose: 0,
-      concealedKong: 0,
-      cannon: 0
-    };
   }
 };
