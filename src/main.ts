a2d9cee708fe930763aad76894c5a85125ebc495
changbin.wangcb <changbin.wangcb@alibaba-inc.com>
2018-03-05 16:57:36 +0800

    export

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
-import './tile';
-import './player';
-import './playerDetail';
-import './rules/basic';
+// import './tile';
+// import './player';
+import Player from './playerDetail';
+// import './rules/basic';
+// import './ai/basic';
+import Game from './game';
+
+// const game = new Game();
+// game.addPlayer(1, 'test1', true);
+// game.addPlayer(2, 'test2', true);
+// game.addPlayer(3, 'test3', true);
+// game.addPlayer(4, 'test4', true);
+// game.start();
+
+// const player = new Player(1, 'AI', 0);
+// player.handTiles = [12, 12, 14, 14, 16, 17, 18, 21, 21, 22, 25, 25, 27, 29];
+// canReadyHand(player);
+
+export default {
+  Player,
+  Game
+};
