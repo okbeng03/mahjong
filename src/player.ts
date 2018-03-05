ac4bf4cc0b99a323d05dd7f4acff1f8cbd343e92
changbin.wangcb <changbin.wangcb@alibaba-inc.com>
2018-03-05 16:58:35 +0800

    AI

--- src/player.ts
+++ src/player.ts
@@ -2,10 +2,12 @@ export default class Player {
   readonly id: number;
   readonly name: string;
   readonly pick: number;
+  readonly isAI: boolean;
 
-  constructor(id: number, name: string, pick: number) {
+  constructor(id: number, name: string, pick: number, isAI: boolean = false) {
     this.id = id;
     this.name = name;
     this.pick = pick;
+    this.isAI = isAI;
   }
 };
